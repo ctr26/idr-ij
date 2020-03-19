805d75e583fb1e0f563fe5a01878351f36e8c1e8
ctr26 <ctr26@ebi.ac.uk>
2020-03-19 09:15:52 +0000

    Expanded Images

--- src/main/java/ebi/idr_py/Images.java
+++ src/main/java/ebi/idr_py/Images.java
@@ -1,10 +1,43 @@
 package ebi.idr_py;
 
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.omero.OMEROService;
+import omero.ServerError;
+import omero.client;
 import omero.gateway.Gateway;
 import omero.gateway.SecurityContext;
+import omero.gateway.exception.DSAccessException;
+import omero.gateway.exception.DSOutOfServiceException;
+import omero.gateway.facility.BrowseFacility;
+import omero.gateway.model.ImageData;
+import org.scijava.Context;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
 
 public class Images {
-    public void images_by_phenotype(SecurityContext context, Gateway gateway, String phentotype){
+    private static String value = "CMPO_0000077";
+    private static String key="Phenotype Term Accession";
+    private static String ns="openmicroscopy.org/mapr/phenotype";
+    private static Long image_id = 2966725L;
 
+    public static Collection<ImageData> images_by_phenotype(SecurityContext context, Gateway gateway, String phenotype) throws ExecutionException, DSAccessException, DSOutOfServiceException {
+        Collection<Long> ann_ids = Attributes.annotation_ids_by_field(context, gateway, phenotype, key, ns);
+        BrowseFacility browse = gateway.getFacility(BrowseFacility.class);
+        return browse.getImages(context, ann_ids);
     }
+
+    public static ImageData get_image(SecurityContext context, Gateway gateway, Long image_id) throws ExecutionException, DSAccessException, DSOutOfServiceException {
+        BrowseFacility browse = gateway.getFacility(BrowseFacility.class);
+        return browse.getImage(context,image_id);
+    }
+
+    public static Dataset get_ij_dataset(ImageJ ij, client idr_client, Long ImageID) throws IOException, ServerError {
+        Context context = ij.getContext();
+        OMEROService dos = context.service(OMEROService.class);
+        return dos.downloadImage(idr_client, ImageID);
+    }
+
 }
