02ac6de0420b5f96613feb81aae1baece01fc4f3
ctr26 <ctr26@ebi.ac.uk>
2020-03-26 12:48:38 +0000

    Added gui, no longer connecting to IDR

---
+++ src/main/java/ebi/idr_py/GUI.java
@@ -0,0 +1,16 @@
+package ebi.idr_py;
+
+import javax.swing.*;
+
+public class GUI {
+    private JTabbedPane tabbedPane1;
+    private JList imageList;
+    private JButton openImages;
+    private JButton populateList;
+    private JPanel oneImage;
+    private JSpinner oneImageLong;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
