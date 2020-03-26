f55e666864b905e066d38848cc6a252f34854210
ctr26 <ctr26@ebi.ac.uk>
2020-03-26 12:29:04 +0000

    Cleaning up Attributes

--- src/main/java/ebi/idr_py/Attributes.java
+++ src/main/java/ebi/idr_py/Attributes.java
@@ -38,55 +38,27 @@ public class Attributes {
 
     public static List<Long> annotation_ids_by_field(SecurityContext context, Gateway gateway,
                                                         String value,String key,String ns){
-//        R = new RType();
-//        String a = R.ice_id();
 
-//        RType value= RType.ice_id("CMPO_0000077");
-//        String value = RString("CMPO_0000077");
-//        String value = RString.ice_staticId();
-
-//        IceMapper mapper = new IceMapper();
         try {
-////            RType value = mapper.toRType("CMPO_0000077");
-//            RType r_value = rtypes.wrap(value);
-////            RType key = mapper.toRType("Phenotype Term Accession");
-//            RType r_key = rtypes.wrap(key);
-////            RType ns = mapper.toRType("openmicroscopy.org/mapr/phenotype");
-//            RType r_ns = rtypes.wrap(ns);
-
             ParametersI params = new ParametersI();
             params.add("value", rtypes.wrap(value));
             params.add("key", rtypes.wrap(key));
             params.add("ns", rtypes.wrap(ns));
         String q = ("select a.id from MapAnnotation a join a.mapValue as mv where a.ns = :ns and mv.name = :key and mv.value = :value");
-//            IQueryPrx b = gateway.getQueryService(context);
-//        List<List<RType>> a = gateway.getQueryService(context).projection(q, params).get(0);
-//        List<List<String>> out = (List<List<String>>) rtypes.unwrap((RType) a);
-//            List<String> c = out.get(0);
-
 //            System.out.println(gateway.getQueryService(context).projection(q, params));
             List<RType> projections = gateway.getQueryService(context).projection(q, params).get(0);
 //            System.out.println(projections);
-//            Object projection = rtypes.unwrap(projections.get(0));
-//            System.out.println(projection);
-
-            List<Long> out = projections.stream().map((n) -> (Long) rtypes.unwrap(n)).collect(Collectors.toList());
-            return out;
-//        unwrap(conn.getQueryService().projection(q, params))[0]
+            return projections.stream().map((n) -> (Long) rtypes.unwrap(n)).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        //        String key="Phenotype Term Accession";
-//        String ns="openmicroscopy.org/mapr/phenotype";
-
-//        params.add("key", key);
-//        params.add("ns", ns);
-//        q = ("select a.id from MapAnnotation a join a.mapValue as mv where a.ns = :ns and mv.name = :key and mv.value = :value");
-//        gateway.getQueryService(context).projection(q,params);
-//        unwrap(conn.getQueryService().projection(q, params))[0]
         return null;
     }
+
+//    public static get_phenotypes_for_gene(session,
+//                                              gene_name,
+//                                              screenid=None,
+//                                              idr_base_url="https://idr.openmicroscopy.org"):
 //                                value="CMPO_0000077",
 //                                key="Phenotype Term Accession",
 //                                ns="openmicroscopy.org/mapr/phenotype"):
